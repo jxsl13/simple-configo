424039bcac1f8e0212ec23e0ebd28676275105f2
jxsl13 <jxsl13@gmail.com>
2021-07-02 12:57:41 +0200

    fix ReadDotEnv, as the last version did not comply with the actual simple-configo Parsing logic.

--- parsers/files.go
+++ parsers/files.go
@@ -9,21 +9,70 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// ReadDotEnvFile reads the provided files and sets the out variable to the expected key provided in the surrounding option.
-// The key then is used to lookup in the files.
-func ReadDotEnvFile(out *string, filePaths ...*string) configo.ParserFunc {
-	return func(key string) error {
-		lookupPaths := make([]string, 0, len(filePaths))
-		for _, p := range filePaths {
-			lookupPaths = append(lookupPaths, *p)
-		}
-		env, err := godotenv.Read(lookupPaths...)
+//DotEnvTuple is used as a bridging struct that connects Keys found in a .env file and the corresponding destination values
+// where the values of these keys are written into.
+type DotEnvTuple struct {
+	// Key .env -> USERNAME=xxx, username is the key
+	Key string
+	// KeyPtr is the dynamic version of the key that might be used to change the Key at runtime.
+	KeyPtr *string
+	//OutValuePtr is the target string variable that receives the value parsed from the .env file.
+	OutValuePtr *string
+}
+
+// ReadDotEnvFileMulti reads the provided file and sets the DotEnvTuple.Value variable to the value found in the .env file
+// under the DotEnvTuple.Key.
+func ReadDotEnvFileMulti(outTuples ...DotEnvTuple) configo.ParserFunc {
+	return func(filePath string) error {
+
+		env, err := godotenv.Read(filePath)
 		if err != nil {
 			return err
 		}
-		// set out to expected value, will be empty in case the value was not found.
-		*out = env[key]
+		for _, tuple := range outTuples {
+			keyPtr := tuple.KeyPtr
+			key := tuple.Key
+			outPtr := tuple.OutValuePtr
+
+			if keyPtr != nil {
+				*outPtr = env[*keyPtr]
+			} else {
+				// set out to expected value, will be empty in case the value was not found.
+				*outPtr = env[key]
+			}
+		}
+
 		return nil
+	}
+}
+
+// ReadDotEnvFileMulti reads the provided file and sets the DotEnvTuple.Value variable to the value found in the .env file
+// under the DotEnvTuple.Key.
+func ReadDotEnvFileMap(outMap map[string]*string) configo.ParserFunc {
+	return func(filePath string) error {
+		tuples := make([]DotEnvTuple, 0, len(outMap))
+		for key, outPtr := range outMap {
+			tuples = append(tuples, DotEnvTuple{
+				Key:         key,
+				OutValuePtr: outPtr,
+			})
+		}
+		f := ReadDotEnvFileMulti(tuples...)
+		return f(filePath)
+	}
+}
+
+// ReadDotEnvFile reads a single variable from the .env file provided in the surrounding option's DefaultValue or from the environment.
+// This functions allows to fetch a single value from the file.
+// If you want to fetch multiple values at once, you may want to try either the easy one: ReadDotEnvFileMap
+// or the slightly more complex version: ReadDotEnvFileMulti
+func ReadDotEnvFile(out *string, key string) configo.ParserFunc {
+	return func(filePath string) error {
+		f := ReadDotEnvFileMulti(DotEnvTuple{
+			Key:         key,
+			OutValuePtr: out,
+		})
+		return f(filePath)
 	}
 }
 
