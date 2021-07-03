e932d4622e1d46f51f2e9b647abdae2d7604e8f3
jxsl13 <jxsl13@gmail.com>
2021-07-03 16:04:23 +0200

    add MapReverse parsing value->key association

--- parsers/collections.go
+++ parsers/collections.go
@@ -92,6 +92,44 @@ func Map(out *map[string]string, pairDelimiter, keyValueDelimiter *string) confi
 	}
 }
 
+// MapReverse allows to define a value->key associations directly inside of a single parameter
+// This might be useful if you want to construct a two way mapping that is able to associate a key to a value as well as
+// the association form a value back to its key.
+func MapReverse(out *map[string]string, pairDelimiter, keyValueDelimiter *string) configo.ParserFunc {
+	return func(value string) error {
+		if *pairDelimiter == *keyValueDelimiter {
+			return fmt.Errorf("pairDelimiter and keyValueDelimiter must not be equal: '%s'", *pairDelimiter)
+		}
+		pairs := strings.Split(value, *pairDelimiter)
+		m := make(map[string]string, len(pairs))
+		for _, pair := range pairs {
+			list := strings.Split(pair, *keyValueDelimiter)
+			if len(list) != 2 {
+				return fmt.Errorf("'%s' is not a key value pair with delimiter '%s'", pair, *keyValueDelimiter)
+			}
+
+			// the only difference between Map & MapReverse
+			key := list[1]
+			value := list[0]
+
+			if _, ok := m[key]; ok {
+				return fmt.Errorf("duplicate key detected: %s", key)
+			}
+
+			m[key] = value
+		}
+
+		if *out == nil {
+			*out = make(map[string]string, len(pairs))
+		}
+
+		for key, value := range m {
+			(*out)[key] = value
+		}
+		return nil
+	}
+}
+
 // MapFromKeysSlice fills the 'out' map with the keys and for each key the corresponding
 // value at the exact same position of the passed "value" string that is split into another
 // list is creates. keys{0, 1, 2, 3} -> values{0, 1, 2, 3}
