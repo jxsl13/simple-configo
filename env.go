1bafa0c264cce2bc88a814bf5b2d2c60335bf313
jxsl13 <jxsl13@gmail.com>
2021-07-04 12:59:23 +0200

    do care about env value that have not value

--- env.go
+++ env.go
@@ -11,8 +11,7 @@ func GetEnv() map[string]string {
 	env := make(map[string]string, len(pairs))
 	for _, pair := range pairs {
 		keyPairs := strings.SplitN(pair, "=", 2)
-		if len(keyPairs) == 2 && keyPairs[1] != "" {
-			// do not care about variables that do not have a value
+		if len(keyPairs) == 2 {
 			env[keyPairs[0]] = keyPairs[1]
 		}
 	}
