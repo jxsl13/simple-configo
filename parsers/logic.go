e08d97f17c82649f416779a1f049be60f1e744b5
jxsl13 <jxsl13@gmail.com>
2021-08-17 12:11:20 +0200

    update error formating

--- parsers/logic.go
+++ parsers/logic.go
@@ -12,7 +12,7 @@ func fmtErr(prefix string, errs []error) error {
 	// format errors
 	cErr := errors.New(prefix)
 	for _, err := range errs {
-		cErr = fmt.Errorf("%w\n -%v", cErr, err)
+		cErr = fmt.Errorf("%w\n - %v", cErr, err)
 	}
 	return cErr
 }
